95c4d0be0eceb9b95a8a89cf86214f57aaf5c151
Taeyoon <naming0617@gmail.com>
2021-12-29 02:41:31 +0900

    feat : 부트스트랩 추가

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,36 +1,7 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import request from "request"
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  },
-  mounted() {
-    request('http://localhost:8080/api/hello', function (error, response, body) {
-      console.log('error : '+error);
-      console.log('response : '+response);
-      console.log('body :'+body);
-    });
-  }
-}
+export default {};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
