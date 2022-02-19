ad8d5788df1ef1158f219d6b399cb7e95e06d804
Taeyoon <naming0617@gmail.com>
2022-02-19 22:25:37 +0900

    feat(SimpleTable.vue) :  게시판 클릭 이벤트 메소드 추가

--- src/frontend/src/components/Tables/SimpleTable.vue
+++ src/frontend/src/components/Tables/SimpleTable.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <md-table v-model="boards" :table-header-color="tableHeaderColor">
-      <md-table-row slot="md-table-row" slot-scope="{ item }">
-        <md-table-cell md-label="번호">{{ item.boardNo }}</md-table-cell>
-        <md-table-cell md-label="제목">{{ item.boardTitle }}</md-table-cell>
+      <md-table-row slot="md-table-row" slot-scope="{ item }" v-on:click="getBoardDetail(item.boardNo)">
+        <md-table-cell md-label="번호" >{{ item.boardNo }}</md-table-cell>
+        <md-table-cell md-label="제목" >{{ item.boardTitle }}</md-table-cell>
       </md-table-row>
     </md-table>
   </div>
@@ -27,12 +27,14 @@ export default {
   },
   methods: {
     getBoardList(){
-      console.log(">>>>> getBoardList");
-      axios.get("/boards/list")
-      .then(response => {
+      axios.get("/boards/list").then(response => {
         this.boards = response.data;
         console.log(this.boards);
       })
+    },
+    getBoardDetail(boardNo){
+      console.log('>>> click event');
+      console.log(boardNo);
     }
   },
   mounted() {
