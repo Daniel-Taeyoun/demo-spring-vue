44346ce98fa7e5654049ae6299d731a820e6bf82
Taeyoon <naming0617@gmail.com>
2022-03-01 16:12:05 +0900

    feat(main.js) : Bootstrap 추가

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,22 +1,11 @@
-// =========================================================
-// * Vue Material Dashboard - v1.5.0
-// =========================================================
-//
-// * Product Page: https://www.creative-tim.com/product/vue-material-dashboard
-// * Copyright 2019 Creative Tim (https://www.creative-tim.com)
-// * Licensed under MIT (https://github.com/creativetimofficial/vue-material-dashboard/blob/master/LICENSE.md)
-//
-// * Coded by Creative Tim
-//
-// =========================================================
-//
-// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App";
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // router setup
 import routes from "./routes/routes";
@@ -45,6 +34,7 @@ Vue.use(MaterialDashboard);
 Vue.use(GlobalComponents);
 Vue.use(GlobalDirectives);
 Vue.use(Notifications);
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
